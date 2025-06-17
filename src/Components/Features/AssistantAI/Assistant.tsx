909427e8987dc401f60da7cf2faf674458605d59
Julian Cortes <mmm808383@gmail.com>
2025-06-17 07:08:40 -0500

    Inicio del apartado del asistente

--- src/Components/Features/AssistantAI/Assistant.tsx
+++ src/Components/Features/AssistantAI/Assistant.tsx
@@ -2,25 +2,31 @@ import { Sidebar } from "../../Ui/UiDashBoard/SideBar";
 import { useState } from "react";
 
 export const Assistant = () => {
+  // Sidebar
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-       // Sidebar
-          const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-        
-          const toggleSidebar = () => {
-            setIsSidebarOpen(!isSidebarOpen);
-          };
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
 
   return (
     <>
-        <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-    
-    <div>
-      <h1 className="text-2xl font-bold mb-4">Asistente de IA</h1>
-      <p className="text-gray-400">
-        Aquí puedes interactuar con el asistente de inteligencia artificial para obtener ayuda.
-      </p>
-    </div>
-    </>
-  )
-}
+      <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
 
+      <div
+        className={`
+          flex-1 p-6 transition-all duration-300 ease-in-out
+          ${
+            isSidebarOpen ? "ml-64" : "ml-20"
+          } /* Ajusta 'ml-20' si tu sidebar cerrada es de otro ancho */
+        `}
+      >
+        <h1 className="text-2xl font-bold mb-4">Asistente de IA</h1>
+        <p className="text-gray-400">
+          Aquí puedes interactuar con el asistente de inteligencia artificial
+          para obtener ayuda.
+        </p>
+      </div>
+    </>
+  );
+};
