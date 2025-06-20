9195a7d04fc0ea000f5ea987216df9a26821a3f0
Julian Cortes <mmm808383@gmail.com>
2025-06-19 22:38:33 -0500

    Realizamos cambios en assistant y creamos el chatBot, para que el ususario pueda interactuar con ellos

--- src/Components/Features/AssistantAI/Assistant.tsx
+++ src/Components/Features/AssistantAI/Assistant.tsx
@@ -1,5 +1,7 @@
 import { Sidebar } from "../../Ui/UiDashBoard/SideBar";
 import { useState } from "react";
+import FinanceAssistant from "./ChatAssitant";
+
 
 export const Assistant = () => {
   // Sidebar
@@ -15,17 +17,20 @@ export const Assistant = () => {
 
       <div
         className={`
-          flex-1 p-6 transition-all duration-300 ease-in-out
+          h-screen flex flex-col p-6 transition-all duration-300 ease-in-out bg-[#020817]
           ${
             isSidebarOpen ? "ml-64" : "ml-20"
           } /* Ajusta 'ml-20' si tu sidebar cerrada es de otro ancho */
         `}
       >
-        <h1 className="text-2xl font-bold mb-4">Asistente de IA</h1>
+        <h1 className="text-2xl font-bold mb-4 text-white">Asistente de IA</h1>
         <p className="text-gray-400">
           Aquí puedes interactuar con el asistente de inteligencia artificial
           para obtener ayuda.
         </p>
+
+
+        <FinanceAssistant />
       </div>
     </>
   );
