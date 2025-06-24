cdec15079d6579efd7ff6c506cff9ec72e877604
Julian Cortes <mmm808383@gmail.com>
2025-06-23 21:03:57 -0500

    Realizamos min-hscreen para que el componente ocupe el 100% del panatlla de manera que se vea estetico haciendolo visualmente agradable para el usuario

--- src/Components/Features/Accounts/Accounts.tsx
+++ src/Components/Features/Accounts/Accounts.tsx
@@ -519,7 +519,7 @@ const Account = () => {
 
       <div
         className={`
-          flex-1 p-6 transition-all duration-300 ease-in-out text-white
+          flex-1 p-6 transition-all duration-300 ease-in-out text-white bg-[#020817] min-h-screen
           ${isSidebarOpen ? "ml-64" : "ml-20"}
         `}
       >
