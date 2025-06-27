4b484a2d152948327717ad6d829704ee8dbc256d
Julian Cortes <mmm808383@gmail.com>
2025-06-27 11:23:16 -0500

    Realizamos el responsive del aparatado de educación financiera

--- src/Components/Features/AssistantAI/Assistant.tsx
+++ src/Components/Features/AssistantAI/Assistant.tsx
@@ -1,6 +1,7 @@
 import { Sidebar } from "../../Ui/UiDashBoard/SideBar";
 import { useState } from "react";
 import FinanceAssistant from "./ChatAssitant";
+import { Menu } from "lucide-react";
 
 export const Assistant = () => {
   // Sidebar
@@ -16,18 +17,27 @@ export const Assistant = () => {
 
       <div
         className={`
-          h-screen flex flex-col p-6 transition-all duration-300 ease-in-out bg-[#020817]
+          min-h-screen flex flex-col sm:p-6 lg:p-8 transition-all text-white duration-300 ease-in-out bg-[#020817]
           ${
-            isSidebarOpen ? "ml-64" : "ml-20"
+            isSidebarOpen ? "lg:ml-64" : "lg:ml-20"
           } /* Ajusta 'ml-20' si tu sidebar cerrada es de otro ancho */
         `}
       >
         <div className="flex items-center gap-4 px-4 pt-8">
+          <div className="flex justify-between items-center mb-20 lg:hidden">
+            <button
+              onClick={toggleSidebar}
+              className="p-2 rounded-md border border-gray-600 hover:bg-gray-800"
+              aria-label="Abrir menú de navegación"
+            >
+              <Menu className="w-5 h-5" />
+            </button>
+          </div>
           <div>
-            <h1 className="text-4xl font-bold mb-2 text-white">
+            <h1 className="text-2xl md:text-4xl font-bold mb-2 text-white">
               Asistente de IA
             </h1>
-            <p className="text-white/70 mb-12 text-lg">
+            <p className="text-white/70 mb-12 text-sm sm:text-lg">
               Aquí puedes interactuar con el asistente de inteligencia
               artificial para obtener ayuda.
             </p>
--- src/Components/Features/FinancialEducation/Education.tsx
+++ src/Components/Features/FinancialEducation/Education.tsx
@@ -1,8 +1,7 @@
 import { Sidebar } from "../../Ui/UiDashBoard/SideBar";
 import { useState } from "react";
 import { HeaderEducation } from "./HeaderEducation";
-
-
+import { Menu } from "lucide-react";
 
 export const Education = () => {
   // Sidebar
@@ -20,12 +19,31 @@ export const Education = () => {
 
       <div
         className={`
-          flex-1 p-6 transition-all duration-300 ease-in-out bg-[#020817] text-white h-full
+          flex-1 sm:p-6 lg:p-8 transition-all duration-300 ease-in-out bg-[#020817] text-white min-h-screen
           ${
-            isSidebarOpen ? "ml-64" : "ml-20"
+            isSidebarOpen ? "lg:ml-64" : "lg:ml-20"
           } /* Ajusta el margen izquierdo según el estado del Sidebar */
         `}
       >
+        <div className="flex items-center px-4 gap-4 pt-8 mb-6">
+          <div className="flex justify-between items-center mb-25 lg:hidden">
+            <button
+              onClick={toggleSidebar}
+              className="p-2 rounded-md border border-gray-600 hover:bg-gray-800"
+              aria-label="Abrir menú de navegación"
+            >
+              <Menu className="w-5 h-5" />
+            </button>
+          </div>
+          <div>
+            <h1 className="text-2xl md:text-4xl font-bold mb-2">Educación Financiera</h1>
+            <p className="mb-12 text-white/70 text-sm sm:text-lg">
+              Aprende conceptos financieros, recibe consejos personalizados y
+              mejora tus habilidades para tomar decisiones financieras
+              inteligentes.
+            </p>
+          </div>
+        </div>
         <HeaderEducation />
       </div>
     </>
