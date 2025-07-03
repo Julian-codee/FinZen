edd763bea2cf7bbb9e886290cba858c77f15647e
Julian Cortes <mmm808383@gmail.com>
2025-07-03 18:15:12 -0500

    Realziamos cambios

--- src/Components/Features/Accounts/Accounts.tsx
+++ src/Components/Features/Accounts/Accounts.tsx
@@ -556,7 +556,7 @@ const Account = () => {
           ))}
           {grouped["Otros"] && grouped["Otros"].length > 0 && (
             <p className="text-yellow-500">
-              Algunas cuentas no tienen un tipo válido y se han clasificado como "Otros".
+            
             </p>
           )}
         </div>
