8ecfb2a0f68a0113c433c734a0d8571c3ad94b03
Julian Cortes <mmm808383@gmail.com>
2025-07-06 11:52:10 -0500

    Realizamos cambios

--- src/Components/Features/AssistantAI/ChatAssitant.tsx
+++ src/Components/Features/AssistantAI/ChatAssitant.tsx
@@ -86,7 +86,7 @@ export default function FinanceAssistantChat() {
 
       if (!response.ok) {
         if (response.status === 401) {
-          localStorage.removeItem("token");
+          localStorage.removeItem("jwtToken");
           setJwtToken(null);
           const errorMessage = "Tu sesión ha expirado o es inválida. Por favor, vuelve a iniciar sesión para continuar.";
           setApiError("Sesión inválida o expirada.");
